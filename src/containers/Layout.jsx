7a7643e182261fc2fffec0460a46521751e5a74d
Taofeeq Oluderu <oluderutaofeeq@gmail.com>
2021-04-17 19:36:19 +0100

    remove axios call, add react lazy

--- src/containers/Layout.jsx
+++ src/containers/Layout.jsx
@@ -1,41 +1,17 @@
 import React from "react";
-import axios from "axios";
 import AllComponents from "./AllComponents";
-import Loading from "../components/Loading";
+import { data } from "../data";
 
 class MainLayaout extends React.Component {
-  state = {
-    loading: true,
-    top: 0,
-    bottom: 10,
-    contact: "",
-    skill: "",
-    project: "",
-  };
+	state = {
+		top: 0,
+		bottom: 10,
+		...data,
+	};
 
-  componentDidMount() {
-    axios
-      .get(`https://meettaofeeq.herokuapp.com/api/`)
-      .then((res) => {
-        console.log("fetching");
-        this.setState({
-          contact: res.data.contact[0],
-          skill: res.data.skill,
-          project: res.data.project,
-        });
-      })
-      .then(() => {
-        this.setState({ loading: false });
-        console.log("fetched");
-      });
-  }
-
-  render() {
-    const isLoading = this.state.loading;
-    return (
-      <>{isLoading ? <Loading /> : <AllComponents content={this.state} />}</>
-    );
-  }
+	render() {
+		return <AllComponents content={this.state} />;
+	}
 }
 
 export default MainLayaout;
